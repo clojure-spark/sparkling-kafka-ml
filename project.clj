97b57e3b259530768c7d3119df7678e859babb56
Steve Chan <chanshunli@gmail.com>
2017-08-21 16:00:27 +0800

    Add base lib

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gorillalabs/sparkling "2.1.2"]
+                 [org.apache.spark/spark-streaming_2.11 "2.2.0"]
+                 [org.apache.spark/spark-streaming-kafka_2.11 "1.6.3"]
+                 [org.apache.spark/spark-streaming-flume_2.11 "2.2.0"]
+                 [org.apache.spark/spark-mllib_2.11 "2.2.0"]]
+  :aot [#".*" sparkling.serialization sparkling.destructuring]
+  :java-source-paths ["src/java"]
+  :main sparkling-kafka-ml.core)
--- src/sparkling_kafka_ml/core.clj
+++ src/sparkling_kafka_ml/core.clj
@@ -1,6 +1,18 @@
-(ns sparkling-kafka-ml.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns sparkling-kafka-ml.core
+  (:require
+   [sparkling.conf :as conf]
+   [sparkling.core :as spark]
+   [sparkling.function :refer [function2]]
+   [sparkling.scalaInterop :as scala]
+   [clojure.tools.logging :as log])
+  (:import (kafka.serializer StringDecoder)
+           (org.apache.spark.api.java JavaSparkContext)
+           (org.apache.spark.streaming Duration)
+           (org.apache.spark.streaming.api.java JavaPairInputDStream)
+           (org.apache.spark.streaming.api.java JavaStreamingContext)
+           (org.apache.spark.streaming.kafka KafkaUtils)
+           (java.util Collections)
+           (java.util HashMap)
+           (java.util Map)
+           (java.util Set))
+  (:gen-class))
